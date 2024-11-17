0e5872c27a9fcfb561ef75cd8e9a8bf7fa5a58ba
ve1zy <111706869+ve1zy@users.noreply.github.com>
2024-11-17 14:45:27 +0600

    Update firstdz.ts

--- firstdz.ts
+++ firstdz.ts
@@ -2,20 +2,23 @@ const args=Deno.args;
 const a: number = Number(args[0]);
 const b: number=Number(args[1]);
 const c: number=Number(args[2]);
-if(a==0){
-    console.log(`Один корень равный ${(-c)/b}`)
-}
-else{
-    const dis: number=b**2-4*a*c;
-    if(dis==0){
-        console.log(`Два одинаковых корня равных ${(-b)/2*a}`)
-    }
-    else if(dis<0){
-        console.log("Нет корней уровнения");
+function schetdis(a: number, b: number, c: number){
+    if(a==0){
+        console.log(`Один корень равный ${(-c)/b}`)
     }
     else{
-        const x1: number = (-b+dis**0.5)/2*a;
-        const x2: number = (-b-dis**0.5)/2*a;
-        console.log(`Первый корень=${x1}, второй=${x2}`);
+        const dis: number=b**2-4*a*c;
+        if(dis==0){
+            console.log(`Два одинаковых корня равных ${(-b)/2*a}`)
+        }
+        else if(dis<0){
+            console.log("Нет корней уровнения");
+        }
+        else{
+            const x1: number = (-b+dis**0.5)/2*a;
+            const x2: number = (-b-dis**0.5)/2*a;
+            console.log(`Первый корень=${x1}, второй=${x2}`);
+        }
     }
 }
+schetdis(a, b, c);
